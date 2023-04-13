df53899f4aa9aa17bc3a9d0a6c2daa32a32a6931
Shahrooz Seifouri <ssayfouri@gmail.com>
2023-04-13 19:19:28 +0430

    add comment on models.py

--- config/blog/models.py
+++ config/blog/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from datetime import datetime
 
 
+# User Model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
@@ -19,6 +20,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
+# Post Model
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
